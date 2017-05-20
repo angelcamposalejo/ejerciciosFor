0f793f6ae42cfd12a9b1b5de9e4e4fb841313921
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-20 09:30:06 -0500

    version que clasifica si un numero menor que 12 es par o impar

--- ejericiosFor/ejericiosFor.cpp
+++ ejericiosFor/ejericiosFor.cpp
@@ -9,16 +9,20 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR cmdLine, int cmdSho
 void ejericiosFor::Window_Open(Win::Event& e)
 {
 	wstring texto;
+	Sys::Format(texto, L"Par\r\tImpar\r\n");
+	tbxSalida.Text += texto;
+	Sys::Format(texto, L"--------------------\r\n");
+	tbxSalida.Text += texto;
 	for (int i = 1; i <= 12; i++)
 	{
 		if (i%2 ==0)
 		{
-			
+			Sys::Format(texto, L"%d\r\n", i);
+			tbxSalida.Text += texto;
 		}
 		else
 		{
-			Sys::Format(texto, L"El cuadrado de %d es: %d\r\n", i, i*i);
-			//tbxSalida.Text+= Sys::Convert::ToString(i);
+			Sys::Format(texto, L"%d\r\t", i);
 			tbxSalida.Text += texto;
 		}
 	}
